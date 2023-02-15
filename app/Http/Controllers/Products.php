33cf361e6e7ae5241544418191a18d9e129d1b7b
jieun0123 <jieun.lee@benefitplus.kr>
2023-02-15 16:40:18 +0900

    [ADD] Product 컨트롤러 생성 / 인라인 블레이드 템플릿 사용

---
+++ app/Http/Controllers/Products.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Blade;
+
+class Products extends Controller
+{
+    //
+    function productList() {
+        $total = 20;
+        return Blade::render('<h1>product list ( {{ $total }} )</h1>', ['total'=>$total]);
+    }
+
+    function addProduct() {
+        $newProduct = 'rose';
+        return Blade::render('<h1>product {{ $newProduct }} is added</h1>', ['newProduct'=>$newProduct]);
+    }
+
+    function updateProduct() {
+        $newProduct = 'rose';
+        return Blade::render('<h1>product {{ $newProduct }} is updated</h1>', ['newProduct'=>$newProduct]);
+    }
+
+    function deleteProduct() {
+        $product = 'rose';
+        return Blade::render('<h1>product {{ $product }} is deleted</h1>', ['product'=>$product]);
+    }
+}
