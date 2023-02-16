d47e647d1410fc51e42a18b910add1867bdd72e8
jieun0123 <jieun.lee@benefitplus.kr>
2023-02-16 13:45:57 +0900

    [FIX] signin validation & error message 생성

--- app/Http/Controllers/Users.php
+++ app/Http/Controllers/Users.php
@@ -8,6 +8,10 @@ class Users extends Controller
 {
   function getUserData(Request $req)
   {
+    $req->validate([
+      'email'=>'required | max:10',
+      'password'=>'required | min:5',
+    ]);
     return $req->input();
   }
 }
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -1,9 +1,28 @@
 <h1>User Signin</h1>
 
-<form action="users" method="POST">
-    @csrf
-    <input type="text" name="email" placeholder="enter user email" /><br><br>
-    <input type="password" name="password" placeholder="enter user PASSWORD" /><br><br>
+<?php 
+  $isErrors = $errors->all(); 
+  if(!empty($isErrors)): 
+?>
 
-    <button type="submit">Signin</button>
+<div style="color:orange;">
+  <h3>ERRORS</h3>
+  <ul>
+    @foreach ( $errors->all() as $error )
+      <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+
+<?php endif; ?>
+
+<form action="users" method="POST">
+  @csrf
+  <input type="text" name="email" placeholder="enter user email" />
+  <p style="color:red;">@error('email'){{ $message }}@enderror</p><br>
+
+  <input type="password" name="password" placeholder="enter user password" />
+  <p style="color:red;">@error('password'){{ $message }}@enderror</p><br>
+
+  <button type="submit">Signin</button>
 </form>
